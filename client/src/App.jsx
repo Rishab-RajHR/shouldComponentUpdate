92320fa09e9bc5b3ff9ba1c02a2c45fdc64618ec
Rishab Raj <rr0948555@gmail.com>
2025-12-12 14:47:51 +0530

    ShouldComponentUpdate

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ShouldComponentUpdate from './ShouldComponentUpdate'
+
+const App = () => {
+  return (
+    <div>
+      <h2>This is the App.jsx file</h2>
+      <ShouldComponentUpdate/>
+    </div>
+  )
+}
+
+export default App
